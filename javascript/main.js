a0c95a710becb5e82454cdf75ba8bb2bdca145c4
Markay <lamvip06192003@gmail.com>
2022-01-28 15:49:21 +0700

    chapter 5

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,9 @@
-let a = `I'm a string`;
-let text = `We are the so-called "Vikings" from the north.`;
-console.log(text);
-console.log(a);
+console.log('Hello world from html'.toUpperCase());
+let a = 5; //int, float, log, double
+let b = 6;
+let c = 6.9;
+let d = '5';
+let e = +d;
+let f = Number(d) //như nhau 
+console.log('e value: ', e, 'type: ', typeof e);
+console.log(a === e, 'a: ', typeof a, 'd: ', typeof d);
