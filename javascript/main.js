3342ae5c28dce3c1fb12b8a71d42afdcb9830242
Markay <lamvip06192003@gmail.com>
2022-01-28 14:27:21 +0700

    chapter 3

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,4 @@
-console.log('Hello world from javascript')
-console.log('Hello world with Markay')
+let x = 6, y = 7, z = 8;
+console.log('Test String', 'x =',x, 'y =',y, 'z =',z );
+
+console.log('Hello world from javascript');
