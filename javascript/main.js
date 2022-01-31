d83643bedbd89273a9f8f5aa7a67a204ca659011
Markay <lamvip06192003@gmail.com>
2022-01-31 15:32:16 +0700

    chapter 12

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,12 @@
 console.log('🚀Hello world from JavaScript🚀');
 let arrTop4 = ['Man city', 'Liverpool', 'MU', 'Chelsea'];
 
-for (let i = 0; i < arrTop4.length; i++) {
-    console.log('Top: ', i + 1, arrTop4[i])
-}
-
-let i = 0
-
-let flag = false;
-
-while (!flag) {
-    console.log('check value i: ', i)
+let i = 0;
+while (i < arrTop4.length) {
     i++;
-    if (i === 10) flag = true;
+    if (arrTop4[i] === 'Chelsea') {
+        console.log('Found it: ', arrTop4[i])
+        continue;
+    }
+    console.log('>>> check i: ', i);
 }
