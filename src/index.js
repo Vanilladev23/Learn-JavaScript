d5a08b06982a7393055dd29201487894ad5a367e
Markay <lamvip06192003@gmail.com>
2022-02-01 22:49:14 +0700

    chapter 15

---
+++ src/index.js
@@ -0,0 +1,13 @@
+console.log('🚀Hello world from JavaScript🚀');
+function ericFunc(x, y, i) {
+  console.log('1');
+  console.log('2');
+  console.log('3');
+  if (i === 5) return;
+
+  console.log('4');
+  console.log('5');
+  return x + y;
+}
+
+console.log('check sum: ', ericFunc(9, 6, 5));
