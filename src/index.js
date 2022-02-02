7936e33d5f05b20384767423de4ff2b226347c0b
Markay <lamvip06192003@gmail.com>
2022-02-02 16:29:11 +0700

    chapter 17

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 console.log('🚀Hello world from JavaScript🚀');
-// function sum(a, b) {
-//     return a + b;
+// // function sum(a, b) {
+//   return a + b;
 // }
 
-//arrow function
+// arrow function
 let sum = (a, b) => {
   return a + b;
 };
+console.log('check sum: ', sum(6, 9));
 
-console.log(' check sum: ', sum(6, 9));
+let obj = {
+  name: 'Kay',
+  address: 'TPHCM',
+  getName: function () {
+    return this.name;
+  },
+};
+console.log('>>> Get Name obj: ', obj.getName());
+// function vs method => reuse
